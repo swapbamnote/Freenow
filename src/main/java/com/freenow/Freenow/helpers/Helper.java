ac3635b9ecd4ee41e195b1dc7628d65709093234
sbamnote <sbamnote@carrentals.com>
2021-02-21 00:39:16 +0100

    get all posts of Samantha

--- src/main/java/com/freenow/Freenow/helpers/Helper.java
+++ src/main/java/com/freenow/Freenow/helpers/Helper.java
@@ -2,6 +2,9 @@ package com.freenow.Freenow.helpers;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.http.HttpStatus;
 
 import com.freenow.Freenow.Endpoints.Endpoints;
@@ -16,13 +19,14 @@ public class Helper {
 	private static final String BASE_URL = ConfigManager.getInstance().getString("baseUrl");
 	private static final String PORT = ConfigManager.getInstance().getString("port");
 	public static int userId;
+	public static List<Integer> postIds = new ArrayList<Integer>();;
 	
 	public Helper() {
 		RestAssured.baseURI = BASE_URL;
 		RestAssured.port = Integer.parseInt(PORT);
 	}
 	
-public Response getAllUsers() {
+ public Response getAllUsers() {
 		
 		Response response = RestAssured
 				.given().log().all()
@@ -35,5 +39,18 @@ public Response getAllUsers() {
 		
 		return response;
 	}
+
+ public Response getAllPosts() {
+	Response response = RestAssured
+			.given().log().all()
+			.contentType(ContentType.JSON)
+			.when()
+			.get(Endpoints.GET_ALL_POSTS)
+			.andReturn();
+
+	assertEquals(response.getStatusCode(), HttpStatus.SC_OK, "OK");
+	
+	return response;
+ }
 
 }
--- src/test/java/com/freenow/Freenow/tests/TestGetCommentsOnPostOfUser.java
+++ src/test/java/com/freenow/Freenow/tests/TestGetCommentsOnPostOfUser.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.freenow.Freenow.model.Users;
 import com.freenow.Freenow.utils.ConfigManager;
+import com.freenow.Freenow.model.Posts;
 import com.freenow.Freenow.helpers.Helper;
 
 import io.restassured.path.json.JsonPath;
@@ -44,6 +45,24 @@ public class TestGetCommentsOnPostOfUser {
 		if(helper.userId ==0) {
 			throw new NullPointerException("User Not found: " + userName);
 		}
+		
+		if(helper.userId !=0) {
+			response = helper.getAllPosts();
+			type = new TypeReference<List<Posts>>() {}.getType();
+			List<Posts> posts = response.as(type);
+			for(int i=0; i<posts.size(); i++) {
+				if(posts.get(i).getUserId() == helper.userId) {
+					int postIds = posts.get(i).getId();
+					helper.postIds.add(postIds);
+				}
+			}
+			if(helper.postIds.isEmpty()) {
+				throw new NullPointerException("No Posts by: "+ ConfigManager.getInstance().getString("userName"));
+			}
+			System.out.println("PostIds "+helper.postIds+ " of Samantha's userId= "+helper.userId);
+		}
+		
+		
 		
 		
 	}
