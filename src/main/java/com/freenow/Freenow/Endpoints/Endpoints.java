634fb9aff9aa14d5c008d652bfe6624c8ea9b19f
sbamnote <sbamnote@carrentals.com>
2021-02-20 21:16:36 +0100

    add required endpoints

--- src/main/java/com/freenow/Freenow/Endpoints/Endpoints.java
+++ src/main/java/com/freenow/Freenow/Endpoints/Endpoints.java
@@ -1,5 +1,10 @@
 package com.freenow.Freenow.Endpoints;
 
 public class Endpoints {
-
+	public static final String GET_ALL_USERS = "/users";
+	public static final String GET_SINGLE_USER = "/users/{id}";
+	public static final String GET_ALL_POSTS = "/posts";
+	public static final String GET_SINGLE_POST = "/posts{id}";
+	public static final String GET_ALL_COMMENTS = "/comments";
+	public static final String GET_SINGLE_COMMENT = "/comments{id}";
 }
