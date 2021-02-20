0515169037d9966ad0d2cbf02f183532fb101a63
sbamnote <sbamnote@carrentals.com>
2021-02-20 21:08:36 +0100

    read config properties file

--- src/main/java/com/freenow/Freenow/utils/ConfigManager.java
+++ src/main/java/com/freenow/Freenow/utils/ConfigManager.java
@@ -1,5 +1,35 @@
 package com.freenow.Freenow.utils;
 
-public class ConfigManager {
+import java.io.FileInputStream;
+import java.util.Properties;
 
+import com.freenow.Freenow.utils.ConfigManager;
+
+public class ConfigManager {
+	
+	private static ConfigManager manager;
+	private static final Properties PROPS = new Properties();
+	
+	private ConfigManager() {
+		try{
+			FileInputStream config = new FileInputStream(System.getProperty("user.dir")+"/resource/config.properties");
+			PROPS.load(config);
+			} catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+	}
+	
+	public static ConfigManager getInstance() {		
+		if(manager == null) {
+			synchronized(ConfigManager.class) {
+				manager = new ConfigManager();
+			}
+		}
+		return manager;
+	}
+	
+	public String getString(String key) {
+		return System.getProperty(key, PROPS.getProperty(key));
+	}
 }
